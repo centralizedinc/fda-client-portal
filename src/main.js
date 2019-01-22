8e2aa7565ff15bfc5f13911a3b9deaf771cfa23b
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-01-22 15:39:55 +0800

    0122

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import '@/assets/css/fdastyle.css'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
@@ -10,21 +11,25 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import NProgress from 'nprogress';
-import '../node_modules/nprogress/nprogress.css' 
+import '../node_modules/nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
-NProgress.configure({ showSpinner: false, trickleRate: 0.5, trickleSpeed: 800 });
+NProgress.configure({
+  showSpinner: false,
+  trickleRate: 0.5,
+  trickleSpeed: 800
+});
 router.beforeResolve((to, from, next) => {
   if (to.name) {
-      NProgress.start()
+    NProgress.start()
   }
   next()
 })
 
 router.afterEach((to, from) => {
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
   NProgress.done()
 })
 
