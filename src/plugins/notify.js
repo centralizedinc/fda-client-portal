1e1c113e34d26476b2efdf21dd199db4ac03c282
Mark <markjhonpaul.quijom@gmail.com>
2019-02-27 14:14:02 +0800

    update

--- src/plugins/notify.js
+++ src/plugins/notify.js
@@ -1,25 +1,24 @@
+export default {
+    install(Vue, store) {
 
-export default{
-    install (Vue, store){
 
-
-        Vue.prototype.$notify = (notification)=>{
+        Vue.prototype.$notify = (notification) => {
             store.commit('ADD_NOTIFICATION', notification);
         }
 
-        Vue.prototype.$notifyError = (errors)=>{
+        Vue.prototype.$notifyError = (errors) => {
             var message = "Oops something went wrong. Please try again."
             var messages = "";
-            if(errors && errors.length > 0){
+            if (errors && errors.length > 0) {
                 errors.forEach(element => {
-                    messages = messages +  element.message + " <br/>"
+                    messages = messages + element.message + " <br/>"
                 });
             }
-            
-            
+
+
             store.commit('ADD_NOTIFICATION', {
-                message: messages ? messages: message,
-                color: 'error',                
+                message: messages ? messages : message,
+                color: 'error',
             });
         }
     }
