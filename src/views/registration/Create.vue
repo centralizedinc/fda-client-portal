8781101e614074a28a9071be5bc3d7bea42d2ce8
Ariel Balita <ariel.a.balita@gmail.com>
2019-02-27 09:03:10 +0800

    update

--- src/views/registration/Create.vue
+++ src/views/registration/Create.vue
@@ -69,7 +69,7 @@
           </v-btn>Get Help
         </v-tooltip>
       </template>
-      <step-six slot="content-step-6" :form="form"></step-six>
+      <step-six slot="content-step-6" :form="form" @upload="uploadFile" style="width: 100%"></step-six>
       <template slot="header-step-7">Account Info
         <v-spacer></v-spacer>
         <v-tooltip left>
@@ -204,7 +204,8 @@ export default {
       username: "",
       password: "",
       name: {}
-    }
+    },
+    formData:{}
   }),
   created() {
     this.init();
@@ -269,6 +270,9 @@ export default {
     },
     changePage(val) {
       this.e1 = val;
+    },
+    uploadFile(upload){
+      this.formData = upload;
     },
     submit() {
       console.log("##### form: " + JSON.stringify(this.form));
