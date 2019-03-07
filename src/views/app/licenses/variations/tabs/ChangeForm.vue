ab3c95680237f61fe2787ec17dca40c51115f633
Mark <markjhonpaul.quijom@gmail.com>
2019-03-07 16:50:03 +0800

    update

--- src/views/app/licenses/variations/tabs/ChangeForm.vue
+++ src/views/app/licenses/variations/tabs/ChangeForm.vue
@@ -1,137 +1,38 @@
 <template>
     <v-container grid-list-xl>
-        <v-layout row wrap>
-          <v-flex xs12 v-for="(item, index) in items" :key="index">
-            <span class="body-1">Old Value</span>: <span class="body-2">{{getOldValue(item.key)}}</span>
-            <!-- Business Name -->
-            <v-text-field 
-              v-if="item.key === 'estab_details.establishment_name'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.estab_details.establishment_name">
-            </v-text-field>
-
-            <!-- Ownership -->
-            <v-text-field
-              v-if="item.key === 'estab_details.establishment_owner'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.estab_details.establishment_owner"
-            ></v-text-field>
-
-            <!-- Address -->
-            <v-text-field
-              v-if="item.key === 'addresses.office.address'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.addresses.office.address"
-            ></v-text-field>
-            
-            <!-- Personel -->
-            <v-text-field
-              v-if="item.key === 'auth_officer.lastname'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.auth_officer.lastname"
-            ></v-text-field>
-
-            <v-text-field
-              v-if="item.key === 'auth_officer.firstname'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.auth_officer.firstname"
-            ></v-text-field>
-
-            <v-text-field
-              v-if="item.key === 'auth_officer.middlename'"
-              :label="item.field"
-              box :rules="[rules.required]"
-              v-model="updatedForm.auth_officer.middlename"
-            ></v-text-field>
-            
-            <v-text-field
-              v-if="item.key === 'auth_officer.email'"
-              :label="item.field"
-              box :rules="[rules.required, rules.email]"
-              v-model="updatedForm.auth_officer.email"
-            ></v-text-field>
-            <v-divider></v-divider>
-          </v-flex>
-        </v-layout>
+      <business-name 
+        v-if="variationContains(['MN4', 'MJ1'])"
+        :existingForm="existingForm"
+        :updatedForm="updatedForm">
+      </business-name>
+      <ownership 
+        v-if="variationContains(['MJ2'])"
+        :existingForm="existingForm"
+        :updatedForm="updatedForm">
+      </ownership>
+      <authorized-personel 
+        v-if="variationContains(['MN5'])"
+        :existingForm="existingForm"
+        :updatedForm="updatedForm">
+      </authorized-personel>
+      <address 
+        v-if="variationContains(['MN4', 'MJ3'])"
+        :existingForm="existingForm"
+        :updatedForm="updatedForm">
+      </address>
     </v-container>
 </template>
 
 <script>
 export default {
+  components: {
+    Address: () => import('./Address.vue'),
+    AuthorizedPersonel: () => import('./AuthorizedPersonel.vue'),
+    BusinessName: () => import('./BusinessName.vue'),
+    Ownership: () => import('./Ownership.vue')
+  },
   props: ["variations", "existingForm", "updatedForm"],
-  data() {
-    return {
-      items: [],
-      businessName: [
-        {
-          field: "Business Name",
-          key: "estab_details.establishment_name"
-        }
-      ],
-      address: [
-        {
-          field: "Address",
-          key: "addresses.office.address"
-        }
-      ],
-      personnel: [
-        {
-          field: "Last Name",
-          key: "auth_officer.lastname"
-        },
-        {
-          field: "First Name",
-          key: "auth_officer.firstname"
-        },
-        {
-          field: "Middle Name",
-          key: "auth_officer.middlename"
-        },
-        {
-          field: "Email",
-          key: "auth_officer.email"
-        }
-      ],
-      ownership: [
-        {
-          field: "Establishment Ownership",
-          key: "estab_details.establishment_owner"
-        }
-      ],
-      rules: {
-        required: v => !!v || "Required field",
-        email: v =>
-          (!!v || "E-mail is required") &&
-          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-          "E-mail must be valid"
-      }
-    };
-  },
-  created() {
-    this.init();
-  },
   methods: {
-    init() {
-      this.items = [];
-
-      // Business Name
-      if (this.variationContains(["MN4", "MJ1"]))
-        this.items = this.items.concat(this.businessName);
-      // Address
-      if (this.variationContains(["MN4", "MJ3"]))
-        this.items = this.items.concat(this.address);
-      // Personnel
-      if (this.variationContains(["MN5"]))
-        this.items = this.items.concat(this.personnel);
-      // Ownership
-      if (this.variationContains(["MJ2"]))
-        this.items = this.items.concat(this.ownership);
-    },
     variationContains(codes) {
       for (let i = 0; i < codes.length; i++) {
         if (this.variations.indexOf(codes[i]) > -1) {
@@ -139,13 +40,6 @@ export default {
         }
       }
       return false;
-    },
-    getOldValue(item_key) {
-      var value = null;
-      item_key.split(".").forEach(key => {
-        value = value ? value[key] : this.existingForm[key];
-      });
-      return value;
     }
   }
 };
