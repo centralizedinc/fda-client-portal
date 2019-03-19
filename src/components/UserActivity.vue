dfd56d90d32be81a7e29cf4a2c1a6b96cfec903e
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-03-19 08:59:42 +0800

    Update issue #1

--- src/components/UserActivity.vue
+++ src/components/UserActivity.vue
@@ -1,15 +1,32 @@
 <template>
-    <v-card class="dcard" height="410">
-        <v-toolbar width="calc(100% - 10px)" class="user-activity-title">
-            <v-toolbar-title>User Activity</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon><v-icon>more_vert</v-icon>
-            </v-btn>
-        </v-toolbar>
+  <v-card class="dcard" height="410">
+    <v-toolbar width="calc(100% - 10px)" class="user-activity-title">
+      <v-toolbar-title>User Activity</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-btn icon><v-icon>more_vert</v-icon>
+      </v-btn>-->
+    </v-toolbar>
 
-        <v-list class="scrollList" three-line width>
-            <template v-for="(item, index) in activities"><v-list-tile :key="index">
-                    <v-list-tile-content>
+    <v-list class="scrollList" three-line width>
+      <template v-for="(item, index) in activities">
+        <v-list-tile :key="index">
+          <v-list-tile-content>
+            <v-list-tile-sub-title class="font-weight-light">{{formatDate(item.date_completed)}}</v-list-tile-sub-title>
+            <v-list-tile-title>
+              {{getTask(item.task_id).name}} on Case No.
+              <v-tooltip bottom>
+                <a slot="activator" @click="viewCase(item.application_id)">{{item.case_no}}</a>
+                View Case
+              </v-tooltip>
+            </v-list-tile-title>
+            <v-list-tile-title class="font-weight-bold">
+              {{getAppType(item.application_type)}} {{getCaseType(item.case_type)}}
+              <span
+                style="text-transform: uppercase"
+              >{{getActStatus(item.status)}}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+          <!-- <v-list-tile-content>
                         <v-list-tile-title class="font-weight-bold">{{getTask(item.task_id).name}}</v-list-tile-title>
                         <v-list-tile-sub-title>{{getActStatus(item.status)}} {{getAppType(item.application_type)}} application of {{getCaseType(item.case_type)}} with</v-list-tile-sub-title>
                         <v-list-tile-sub-title>
@@ -19,12 +36,12 @@
                             View Details
                           </v-tooltip> on {{formatDate(item.date_completed)}}
                         </v-list-tile-sub-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-divider :key="index"></v-divider>
-            </template>
-        </v-list>
-    </v-card>
+          </v-list-tile-content>-->
+        </v-list-tile>
+        <v-divider :key="index"></v-divider>
+      </template>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
