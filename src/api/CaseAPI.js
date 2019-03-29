5e503f3e9c55630361e9afa8e44737ff8d78bdaf
abalita <ariel.a.balita@gmail.com>
2019-03-29 15:40:38 +0800

    update

--- src/api/CaseAPI.js
+++ src/api/CaseAPI.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default class CaseAPI {
     constructor(token) {
-        axios.defaults.baseURL = 'https://fda-services.herokuapp.com/v1.0';
+        axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URI;
         axios.defaults.headers.common['Content-Type'] = 'application/json'
         axios.defaults.headers.common['access_token'] = token;
     }
