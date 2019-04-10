415d658fa680ea1a2c060d342c09401cb8251466
abalita <ariel.a.balita@gmail.com>
2019-04-10 11:22:38 +0800

    update dashboard

--- src/views/app/UserPortfolio_old.vue
+++ src/views/app/UserPortfolio_old.vue
@@ -2,106 +2,19 @@
   <v-layout row wrap align-start>
     <template v-if="user.status === 2">
       <!-- Active License -->
-      <!-- <v-flex xs12 mb-2>
+      <v-flex xs12 mb-2>
         <div class="headline font-weight-thin" style="text-transform: uppercase">Active License</div>
-      </v-flex> -->
-      <v-flex xs12 md3 pa-2 d-flex>
-        <v-card style="background-color: #00a65a!important; box-shadow: 0 6px 20px rgba(101, 198, 187, 1);">          
-            <v-card-text>
-              <v-layout row wrap>
-                <v-flex xs9>
-                  <span class="title white--text font-weight-bold">L1234567890</span>
-                  <div class="caption  white--text  mt-2">Expiry: </div>
-                  <div class="caption  white--text ">Status: </div>
-                  <div class="caption  white--text ">Case No: </div>
-                </v-flex>
-                <v-flex xs3 align-center justify-center d-flex>
-                  <v-icon style="font-size:60px; color:rgba(0,0,0,.15)">fas fa-file-signature</v-icon>
-                </v-flex>
-              </v-layout>                            
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions style="background:rgba(0,0,0,.1)">
-              <v-spacer></v-spacer>
-              <p class="caption white--text" style="cursor:pointer" @click="$router.push('/app/licenses')">More info <v-icon color="white" small>launch</v-icon></p>
-              <v-spacer></v-spacer>
-            </v-card-actions>          
-        </v-card>
       </v-flex>
-      <!-- <active-license></active-license> -->
-      
-      <v-flex xs12 md3 pa-2 d-flex>
-        <v-card style="background:linear-gradient(360deg, #297373 0%, #5FA391 100%); box-shadow: 0 6px 20px rgba(101, 198, 187, 1);">          
-            <v-card-text>
-              <v-layout row wrap>
-                <v-flex xs9>
-                  <span class="display-2 white--text font-weight-bold">0</span>
-                  <p class="title  white--text font-weight-bold mt-2">Certificates</p>
-                </v-flex>
-                <v-flex xs3 align-center justify-center d-flex>
-                  <v-icon style="font-size:60px; color:rgba(0,0,0,.15)">fas fa-certificate</v-icon>
-                </v-flex>
-              </v-layout>                            
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions style="background:rgba(0,0,0,.1)">
-              <v-spacer></v-spacer>
-              <p class="caption white--text" style="cursor:pointer" @click="$router.push('/app/certificates')">More info <v-icon color="white" small>launch</v-icon></p>
-              <v-spacer></v-spacer>
-            </v-card-actions>          
-        </v-card>
-      </v-flex>
-      <v-flex xs12 md3 pa-2 d-flex>
-        <v-card style="background: linear-gradient(360deg, #F1961D 0%, #F58555  100%); box-shadow: 0 6px 20px 0 rgba(228, 120, 51, 1)">          
-            <v-card-text>
-              <v-layout row wrap>
-                <v-flex xs9>
-                  <span class="display-2 white--text font-weight-bold">0</span>
-                  <p class="title  white--text font-weight-bold mt-2">Products</p>
-                </v-flex>
-                <v-flex xs3 align-center justify-center d-flex>
-                  <v-icon style="font-size:60px; color:rgba(0,0,0,.15)">fas fa-file-invoice</v-icon>
-                </v-flex>
-              </v-layout>                            
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions style="background:rgba(0,0,0,.1)">
-              <v-spacer></v-spacer>
-              <p class="caption white--text" style="cursor:pointer" @click="$router.push('/app/certificates')">More info <v-icon color="white" small>launch</v-icon></p>
-              <v-spacer></v-spacer>
-            </v-card-actions>          
-        </v-card>        
-      </v-flex>
-      <v-flex xs12 md3 pa-2 d-flex>
-        <v-card style="background: linear-gradient(360deg, #95A523 0%, #B5C25A  100%); box-shadow: 0 6px 20px 0 rgba(200, 247, 197, 1)">          
-            <v-card-text>
-              <v-layout row wrap>
-                <v-flex xs9>
-                  <span class="display-2 white--text font-weight-bold">0</span>
-                  <p class="title  white--text font-weight-bold mt-2">Payments</p>
-                </v-flex>
-                <v-flex xs3 align-center justify-center d-flex>
-                  <v-icon style="font-size:60px; color:rgba(0,0,0,.15)">fas fa-credit-card</v-icon>
-                </v-flex>
-              </v-layout>                            
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions style="background:rgba(0,0,0,.1)">
-              <v-spacer></v-spacer>
-              <p class="caption white--text" style="cursor:pointer" @click="$router.push('/app/payments')">More info <v-icon color="white" small>launch</v-icon></p>
-              <v-spacer></v-spacer>
-            </v-card-actions>          
-        </v-card>
-      </v-flex>
+      <active-license></active-license>
 
       <!-- Cards -->
-      <!-- <v-flex xs12 mt-5>
+      <v-flex xs12 mt-5>
         <div class="headline font-weight-thin" style="text-transform: uppercase">Overview</div>
       </v-flex>
-      <dashboard-overview></dashboard-overview> -->
-      <!-- <v-flex xs12 mt-5>
+      <dashboard-overview></dashboard-overview>
+      <v-flex xs12 mt-5>
         <div class="headline font-weight-thin" style="text-transform: uppercase">Reference</div>
-      </v-flex> -->
+      </v-flex>
     </template>
 
     <!-- REFERENCE -->
