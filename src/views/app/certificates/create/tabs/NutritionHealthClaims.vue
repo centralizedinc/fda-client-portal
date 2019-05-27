d1663c38529274d5d59ba36604ae2e9fd0053ed8
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-05-27 16:58:00 +0800

    loop necessary fields in form

--- src/views/app/certificates/create/tabs/NutritionHealthClaims.vue
+++ src/views/app/certificates/create/tabs/NutritionHealthClaims.vue
@@ -2,26 +2,37 @@
   <v-form ref="valid">
     <v-container grid-list-md>
       <v-layout row wrap align-center justify-center>
-        <v-flex xs6>
-          <v-autocomplete
-            outline
-            label="Claims"
-            :rules="[rules.required]"
-            hint="Please choose if Nutrition or Health Claims"
-            :items="items"
-            autocomplete
-          ></v-autocomplete>
-        </v-flex>
-        <v-flex xs5>
-          <v-text-field outline name="name" label="Description" id="id"></v-text-field>
-        </v-flex>
-        <v-flex mb-4>
-          <v-tooltip top>
-            <v-btn slot="activator" flat icon color="primary">
-              <v-icon>far fa-plus-square</v-icon>
-            </v-btn>Add New
-          </v-tooltip>
-        </v-flex>
+        <template v-for="(item, index) in items">
+          <v-flex xs6 :key="index">
+            <v-autocomplete
+              outline
+              label="Claims"
+              :rules="[rules.required]"
+              hint="Please choose if Nutrition or Health Claims"
+              :items="items"
+              autocomplete
+            ></v-autocomplete>
+          </v-flex>
+          <v-flex xs5 :key="index">
+            <v-text-field
+              outline
+              name="name"
+              label="Description"
+              v-model="item.desc"
+              :append-outer-icon="index == items.length-1?'far fa-plus-square':''"
+              :append-icon="index != items.length-1?'fas fa-backspace':''"
+              @click:append-outer="addItem"
+              @click:append="removeItem(index)"
+            ></v-text-field>
+          </v-flex>
+          <!-- <v-flex mb-4 :key="index" v-if="index > 0">
+            <v-tooltip top>
+              <v-btn slot="activator" flat icon color="primary" @click="removeItem(index)">
+                <v-icon>far fa-minus-square</v-icon>
+              </v-btn>Remove
+            </v-tooltip>
+          </v-flex>-->
+        </template>
       </v-layout>
     </v-container>
   </v-form>
@@ -35,7 +46,18 @@ export default {
     rules: {
       required: value => !!value || "This field is required"
     }
-  })
+  }),
+  methods: {
+    addItem() {
+      this.items.push({
+        items: "",
+        desc: ""
+      });
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    }
+  }
 };
 </script>
 
