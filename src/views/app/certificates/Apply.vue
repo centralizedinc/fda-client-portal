05d1621f388d9566b2944112f8b77f0bda890498
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-05-30 02:20:08 +0800

    cert

--- src/views/app/certificates/Apply.vue
+++ src/views/app/certificates/Apply.vue
@@ -71,7 +71,7 @@
           </v-stepper-step>
           <v-stepper-content step="6">
             <v-card flat class="mb-5" height="auto">
-              <step-six></step-six>
+              <step-six :form="cert_form"></step-six>
             </v-card>
             <v-btn color="primary" @click="e6 = 7">Continue</v-btn>
             <v-btn flat>Cancel</v-btn>
@@ -163,136 +163,147 @@ export default {
     e6: 1,
     confirmDialog: false,
     cert_form: {
-  //   general_info: {
-  //     application_type: 0,
-  //     for_ammendment_renewal: {
-  //         product_certificate: "",
-  //         registration: "",
-  //         date_issued: "",
-  //         date_validity: ""
-  //     },
-  //     for_reapplication: {
-  //         doctracker_no: "",
-  //         email: ""
-  //     }
-  // },
+      //   general_info: {
+      //     application_type: 0,
+      //     for_ammendment_renewal: {
+      //         product_certificate: "",
+      //         registration: "",
+      //         date_issued: "",
+      //         date_validity: ""
+      //     },
+      //     for_reapplication: {
+      //         doctracker_no: "",
+      //         email: ""
+      //     }
+      // },
 
-  food_product: {
-      type: "",
-      categorization: "",
-      brand_name: "",
-      product_name: "",
-      company: "",
-      address: "",
-      license_no: "",
-      license_validity: "",
-      years_applied: 0,
-      // object nalang contact
-      contacts: {}
-  },
+      food_product: {
+        type: "",
+        categorization: "",
+        brand_name: "",
+        product_name: "",
+        company: "",
+        address: "",
+        license_no: "",
+        license_validity: "",
+        years_applied: 0,
+        // object nalang contact
+        contacts: {}
+      },
 
-  establisment_info: {
-      activity: "",
-      type: "",
-      origin_country: [],
-      directly_source: false,
-      manufacturer_name: "",
-      manufacturer_address: "",
-      supplier_name: "",
-      supplier_address: ""
-  },
+      establisment_info: {
+        activity: "",
+        type: "",
+        origin_country: [],
+        directly_source: false,
+        manufacturer_name: "",
+        manufacturer_address: "",
+        supplier_name: "",
+        supplier_address: ""
+      },
 
-  ingredients: [],
+      ingredients: [],
 
-  product_specification: {
-      physical: {
+      product_specification: {
+        physical: {
           color: "",
           odor: "",
           taste: "",
           texture: "",
           form: ""
+        },
+        chemical: [
+          {
+            parameter: "",
+            specification: ""
+          }
+        ],
+        microbiological: [
+          {
+            parameter: "",
+            specification: ""
+          }
+        ]
       },
-      chemical: [{
-          parameter: "",
-          specification: ""
-      }],
-      microbiological: [{
-          parameter: "",
-          specification: ""
-      }]
-  },
 
-  shelf: {
-      packaging_material: "",
-      description: "",
-      storage_requirements: "",
-      food_material: "",
-      allergen_source: "",
-      lot_code_interpretation: "",
-      // expiration date
-      date: ""
-  },
+      shelf: {
+        packaging_material: "",
+        description: "",
+        storage_requirements: "",
+        food_material: "",
+        allergen_source: "",
+        lot_code_interpretation: "",
+        // expiration date
+        date: ""
+      },
 
-  nutrition_info: {
-      serving_size: "",
-      serving_per_pack: 0,
-      servings: [{
-          type: "",
-          amount_per_serving: "",
-          percent: ""
-      }]
-  },
+      nutrition_info: {
+        serving_size: "",
+        serving_per_pack: 0,
+        servings: [
+          {
+            type: "",
+            amount_per_serving: "",
+            percent: ""
+          }
+        ]
+      },
 
-// array nalang
-  claims: {
-      nutrition: [],
-      health: []
-  },
+      // array nalang
+      claims: {
+        nutrition: [],
+        health: []
+      },
 
-  documentary: {
-      product_label_images: [],
-      commercial_product_images: [],
-      for_export: false,
-      purchase_request_notarized: [],
-      has_claims: false,
-      substantiate_claims: [],
-      has_on_label: false
-  },
+      documentary: {
+        product_label_images: [],
+        commercial_product_images: [],
+        for_export: false,
+        purchase_request_notarized: [],
+        has_claims: false,
+        substantiate_claims: [],
+        has_on_label: false
+      },
 
-  for_ammendment: {
-      brand_name: "",
-      product_name: "",
-      company_name: "",
-      supplier_name: "",
-      supplier_address: "",
-      packaging_material: "",
-      product_commercial: "",
-      shelf_life: "",
-      packaging_design: []
-  }
+      for_ammendment: {
+        brand_name: "",
+        product_name: "",
+        company_name: "",
+        supplier_name: "",
+        supplier_address: "",
+        packaging_material: "",
+        product_commercial: "",
+        shelf_life: "",
+        packaging_design: []
+      }
     }
   }),
   created() {
-    console.log("apply created")
+    console.log("apply created");
     this.init();
   },
 
   methods: {
-    init(){
-//       console.log("this.$store.state.certificate.init_form: " + JSON.stringify(this.$store.state.certificate.init_form));
-// this.cert_form = this.$store.state.certificate.init_form
-//     console.log("created porps: " + JSON.stringify(this.cert_form));
-    
-    console.log("apply certificates")
-      this.$store.dispatch("GET_FOOD_PRODUCT").then(result=>{
-        return this.$store.dispatch("GET_FOOD_CATEGORY")
-      }).then(result => {
-        return this.$store.dispatch("GET_REGION")
-      }).then((result) => {
-        return this.$store.dispatch("GET_SHELF_LIFE")
-      }).catch((err) => {
-        this.$notifyError(err);
-      });
+    init() {
+      //       console.log("this.$store.state.certificate.init_form: " + JSON.stringify(this.$store.state.certificate.init_form));
+      // this.cert_form = this.$store.state.certificate.init_form
+      //     console.log("created porps: " + JSON.stringify(this.cert_form));
+
+      console.log("apply certificates");
+      this.$store
+        .dispatch("GET_FOOD_PRODUCT")
+        .then(result => {
+          return this.$store.dispatch("GET_FOOD_CATEGORY");
+        })
+        .then(result => {
+          return this.$store.dispatch("GET_REGION");
+        })
+        .then(result => {
+          return this.$store.dispatch("GET_SHELF_LIFE");
+        })
+        .catch(err => {
+          this.$notifyError(err);
+        });
     },
     close() {
       this.showAppOverview = false;
--- src/views/app/certificates/create/tabs/NutritionInfo.vue
+++ src/views/app/certificates/create/tabs/NutritionInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-form ref="valid">
     <v-container grid-list-md>
-      <v-layout row wrap align-center justify-center>
+      <v-layout row wrap>
         <v-flex xs6>
           <v-text-field outline :rules="[rules.required]" label="Serving Size (grams or ml)"></v-text-field>
         </v-flex>
@@ -13,95 +13,144 @@
           ></v-text-field>
         </v-flex>
         <!-- amt per serving -->
+        <v-flex xs12>
+          <v-card>
+            <v-data-table :headers="headers" :items="nutritionServing" hide-actions>
+              <template v-slot:items="props">
+                <td class="text-xs-left">{{ props.item.nut_info }}</td>
+                <td>
+                  <v-edit-dialog
+                    large
+                    :return-value.sync="props.item.aps"
+                    lazy
+                    @save="save"
+                    @cancel="cancel"
+                    @open="open"
+                    @close="close"
+                  >
+                    {{ props.item.aps }}
+                    <template v-slot:input>
+                      <v-text-field
+                        hint="Input corresponding amount per serving"
+                        v-model="props.item.aps"
+                        label="Amount per serving"
+                        single-line
+                        :rules="[rules.required, rules.counter]"
+                        maxlength="5"
+                      ></v-text-field>
+                    </template>
+                  </v-edit-dialog>
+                </td>
 
-        <v-flex xs6>
-          <v-autocomplete
-            outline
-            :rules="[rules.required]"
-            hint="Please add all the fields in the list and input corresponding %RENI"
-            persistent-hint
-            label="1. Amount Per Serving"
-            :items="amtServing"
-            autocomplete
-          ></v-autocomplete>
+                <td class="text-xs-left">
+                  <v-edit-dialog
+                    :return-value.sync="props.item.reni"
+                    large
+                    lazy
+                    @save="save"
+                    @cancel="cancel"
+                    @open="open"
+                    @close="close"
+                  >
+                    <div>{{ props.item.reni }}</div>
+                    <template v-slot:input>
+                      <v-text-field
+                        v-model="props.item.reni"
+                        label="%RENI"
+                        hint="N/A if not applicable"
+                        single-line
+                        :rules="[rules.required, rules.counter]"
+                        maxlength="5"
+                      ></v-text-field>
+                    </template>
+                  </v-edit-dialog>
+                </td>
+              </template>
+            </v-data-table>
+          </v-card>
         </v-flex>
-        <v-flex xs5>
-          <v-text-field
-            outline
-            :rules="[rules.required]"
-            name="name"
-            hint="N/A if not applicable"
-            label="%RENI, for Locally Manufactured Products "
-            id="id"
-          ></v-text-field>
-        </v-flex>
-        <v-flex mb-4>
-          <v-tooltip top>
-            <v-btn slot="activator" flat icon color="primary">
-              <v-icon>far fa-plus-square</v-icon>
-            </v-btn>Add New
-          </v-tooltip>
-        </v-flex>
-
-        <!-- vits -->
-        <v-flex xs6>
-          <v-autocomplete
-            :rules="[rules.required]"
-            outline
-            label="2. Vitamins"
-            :items="vitamins"
-            hint="Please add all the fields in the list and input corresponding %RENI"
-            persistent-hint
-            autocomplete
-          ></v-autocomplete>
-        </v-flex>
-        <v-flex xs5>
-          <v-text-field
-            outline
-            :rules="[rules.required]"
-            name="name"
-            hint="N/A if not applicable"
-            label="%RENI, for Locally Manufactured Products "
-            id="id"
-          ></v-text-field>
-        </v-flex>
-        <v-flex mb-4>
-          <v-tooltip top>
-            <v-btn slot="activator" flat icon color="primary">
-              <v-icon>far fa-plus-square</v-icon>
-            </v-btn>Add New
-          </v-tooltip>
+        <v-snackbar :top="y === 'top'" v-model="snack" :timeout="3000" :color="snackColor">
+          {{ snackText }}
+          <v-btn flat @click="snack = false">Close</v-btn>
+        </v-snackbar>
+        <br>
+        <!-- Vitamins & Minerals -->
+        <v-flex xs12>
+          <v-toolbar dark flat color="primary" class="elevation-5">
+            <span class="title font-weight-light">Vitamins & Minerals</span>
+            <v-spacer></v-spacer>
+            <v-tooltip top>
+              <v-btn slot="activator" outline small icon @click="addNew">
+                <v-icon>edit</v-icon>
+              </v-btn>Add New
+            </v-tooltip>
+          </v-toolbar>
         </v-flex>
 
-        <!-- minerals -->
-        <v-flex xs6>
-          <v-autocomplete
-            outline
-            :rules="[rules.required]"
-            label="3. Minerals"
-            hint="Please add all the fields in the list and input corresponding %RENI"
-            persistent-hint
-            :items="minerals"
-            autocomplete
-          ></v-autocomplete>
+        <v-flex xs12>
+          <v-data-table :headers="headers" :items="v_m" hide-actions class="elevation-1">
+            <template slot="items" slot-scope="props">
+              <tr @click="viewItem(props.item, props.index)" style="cursor:pointer">
+                <td>{{props.item.nut_info}}</td>
+                <td>{{props.item.aps}}</td>
+                <td>{{props.item.reni}}</td>
+              </tr>
+            </template>
+          </v-data-table>
         </v-flex>
-        <v-flex xs5>
-          <v-text-field
-            outline
-            :rules="[rules.required]"
-            name="name"
-            hint="N/A if not applicable"
-            label="%RENI, for Locally Manufactured Products "
-            id="id"
-          ></v-text-field>
-        </v-flex>
-        <v-flex mb-4>
-          <v-tooltip top>
-            <v-btn slot="activator" flat icon small color="primary">
-              <v-icon>far fa-plus-square</v-icon>
-            </v-btn>Add New
-          </v-tooltip>
-        </v-flex>
+        <v-dialog v-model="dialogVm" scrollable max-width="500px" transition="dialog-transition">
+          <v-card>
+            <v-toolbar dark color="primary">
+              <span class="title font-weight-light">New</span>
+              <v-spacer></v-spacer>
+              <v-btn flat icon @click="dialogVm=false">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text>
+              <v-form v-model="valid">
+                <v-container grid-list-md>
+                  <v-layout row wrap>
+                    <v-flex xs12>
+                      <v-autocomplete
+                        outline
+                        label="Vitamins & Minerals"
+                        hint="Choose one"
+                        persistent-hint
+                        :items="vm_items"
+                        autocomplete
+                      ></v-autocomplete>
+                    </v-flex>
+
+                    <v-flex xs12>
+                      <v-text-field outline name="name" label="Amount per Serving" id="id"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12>
+                      <v-text-field
+                        outline
+                        name="name"
+                        hint="N/A if not applicable"
+                        label="%RENI, for Locally Manufactured Products "
+                        id="id"
+                      ></v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions v-if="add">
+              <v-spacer></v-spacer>
+              <v-btn outline color="primary" @click="dialogSpecs=false">Cancel</v-btn>
+              <v-btn color="primary">Add</v-btn>
+            </v-card-actions>
+            <v-card-actions v-else>
+              <v-spacer></v-spacer>
+              <v-btn outline color="error">Delete</v-btn>
+              <v-btn color="primary">Edit</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-layout>
     </v-container>
   </v-form>
@@ -111,23 +160,82 @@
 export default {
   props: ["form"],
   data: () => ({
+    add: false,
+    dialogVm: false,
+    pagination: {},
+    y: "top",
+    snack: false,
+    snackColor: "",
+    snackText: "",
     valid: true,
     rules: {
-      required: value => !!value || "This field is required"
+      required: value => !!value || "This field is required",
+      counter: value => value.length <= 5 || "Max 5 characters"
     },
-    amtServing: [
-      "Calories (kcal)",
-      "Total Fat (g)",
-      "Saturated Fat (g)",
-      "Trans Fat (g)",
-      "Cholesterol (mg)",
-      "Sodium (mg)",
-      "Total Carbohydrates (g)",
-      "Sugar (g)",
-      "Dietary Fiber (g)",
-      "Total Protein (g)"
+    headers: [
+      {
+        text: "Nutrition Information",
+        align: "left",
+        value: "nut_info",
+        sortable: false,
+        width: "1px"
+      },
+      {
+        text: "Amount per Serving",
+        sortable: false,
+        width: "1px",
+        value: "aps"
+      },
+      {
+        text: "%RENI for Locally Manufactured Products",
+        value: "reni",
+        sortable: false,
+        width: "1px"
+      }
     ],
-    vitamins: [
+    nutritionServing: [
+      {
+        nut_info: "Calories (kcal)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Total Fat (g)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Saturated Fat (g)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Trans Fat (g)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Cholesterol (mg)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Sodium (mg)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Total Carbohydrates (g)",
+        aps: "",
+        reni: ""
+      },
+      {
+        nut_info: "Sugar (g)",
+        aps: "",
+        reni: ""
+      }
+    ],
+    vm_items: [
       "Vitamin C (mg)",
       "Thiamin (mg)",
       "Riboflavin (mg)",
@@ -138,9 +246,8 @@ export default {
       "Vitamin A RE (IU)",
       "Vitamin D (IU)",
       "Vitamin E (IU)",
-      "Vitamin K (ug)"
-    ],
-    minerals: [
+      "Vitamin K (ug)",
+      { divider: true },
       " Calcium (mg)",
       "Iron (mg)",
       "Iodine (ug)",
@@ -151,9 +258,34 @@ export default {
       "Flouride (mg)",
       "Manganese (mg)"
     ]
-  })
+  }),
+  methods: {
+    addNew() {
+      this.add = true;
+      this.dialogVm = true;
+    },
+    save() {
+      this.snack = true;
+      this.snackColor = "success";
+      this.snackText = "Data saved";
+    },
+    cancel() {
+      this.snack = true;
+      this.snackColor = "error";
+      this.snackText = "Note that this is a required field";
+    },
+    open() {
+      this.snack = true;
+      this.snackColor = "warning";
+      this.snackText = "This is a required field";
+    },
+    close() {
+      console.log("Dialog closed");
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 </style>
+
