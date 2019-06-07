f50e6760877f73e7280bcdd0fe752f7728a38812
Mark <markjhonpaul.quijom@gmail.com>
2019-06-07 16:55:30 +0800

    push notification

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+import {
+  register
+} from 'register-service-worker'
 import axios from 'axios'
 
 /**
@@ -19,11 +21,11 @@ const urlB64ToUint8Array = base64String => {
 }
 
 window.fda_pwa = {
-  
-  applicationServerKey:urlB64ToUint8Array('BBNvP5Z_RO8RnvXMAKHJQC-WUHxF36L2-oxlXbL9-OxdJsEpUK3Ed67KVXwMydf5QvpM8vxfNsDdTaCG0CoQ_Ro'),
-  isSubscribed:false,
+
+  applicationServerKey: urlB64ToUint8Array('BBNvP5Z_RO8RnvXMAKHJQC-WUHxF36L2-oxlXbL9-OxdJsEpUK3Ed67KVXwMydf5QvpM8vxfNsDdTaCG0CoQ_Ro'),
+  isSubscribed: false,
   subscription: {},
-  registration:{},
+  registration: {},
   /**
    * 
    * @param {*} registration 
@@ -35,83 +37,159 @@ window.fda_pwa = {
         this.subscription = subscription;
         this.registration = registration;
         this.isSubscribed = !(subscription === null)
-    })
+        console.log('this.registration :', this.registration);
+      })
+      .catch(error => {
+        console.log('checkSubscription error :', error);
+      })
   },
   /**
    * 
    * @param {String} account_id 
    */
-  askPermission(account_id){
-    console.log('permission: ', Notification.permission )
-    if(!Notification.permission || Notification.permission === 'default'){ 
-      Notification.requestPermission(result => {
-        console.log('result from permission question', result);
-        if(Notification.permission === 'granted'){
-          this.subscribe(account_id)
+  askPermission(account_id) {
+    var _self = this
+
+    _self.getSubscriptionByAccountId(account_id)
+      .then((result) => {
+        if (result.data.model && result.data.model.length) {
+          var subscriptions = result.data.model
+          console.log('subscriptions :', subscriptions);
+          if (Notification.permission === 'granted') {
+            _self.checkLocalSubscription(account_id, subscriptions, _self)
+          } else {
+            Notification.requestPermission(result => {
+              console.log('result from permission question', result);
+              if (Notification.permission === 'granted') {
+                _self.checkLocalSubscription(account_id, subscriptions, _self)
+              }
+            })
+          }
         }
-      })
-    } 
+      }).catch((err) => {
+        console.log('getSubscriptionByAccountId error :', err);
+        reject()
+      });
+  },
+
+  checkLocalSubscription(account_id, subscriptions, _self) {
+    if (_self.subscription) {
+      var isExist = false
+      subscriptions.forEach(sub => {
+        if (sub.subscription.endpoint === _self.subscription.endpoint) isExist = true
+      });
+      if (!isExist) {
+        _self.saveSubscription(account_id, _self)
+          .then((result) => {
+            _self.isSubscribed = true
+            console.log('Done saving subscription :', result);
+          }).catch((err) => {
+            console.log('err :', err);
+          });
+      } else console.log('Subscription already exist.');
+    } else {
+      _self.subscribe(account_id, _self)
+        .then((result) => {
+          console.log('Done saving subscription :', result);
+        }).catch((err) => {
+          console.log('err :', err);
+        });
+    }
+  },
+
+  saveSubscription(account_id, _self) {
+    var details = {
+      subscription: this.subscription,
+      account_id
+    }
+    return axios.post(process.env.VUE_APP_API_BASE_URI + '/notify-api/subscriptions/subscribe', details)
+  },
+
+  getSubscriptionByAccountId(account_id) {
+    return axios.get(process.env.VUE_APP_API_BASE_URI + `/notify-api/subscriptions/subscribe/${account_id}`)
+  },
+
+  /**
+   * 
+   * @param {String} account_id 
+   */
+  subscribe(account_id, _self) {
+    const options = {
+      applicationServerKey: _self.applicationServerKey,
+      userVisibleOnly: true
+    }
+    return new Promise((resolve, reject) => {
+      _self.registration.pushManager.subscribe(options)
+        .then(subscription => {
+          _self.subscription = subscription
+          return _self.saveSubscription(account_id, _self);
+        })
+        .then(result => {
+          _self.isSubscribed = true
+          resolve()
+        })
+        .catch(error => {
+          console.error('error: ', error)
+          reject(error)
+        })
+    })
   },
   /**
    * 
    */
-  subscribe(account_id){
-    const options = {applicationServerKey:this.applicationServerKey, userVisibleOnly: true}
-    this.registration.pushManager.subscribe(options)
-      .then(subscription=>{
-        console.log('subscription: ', JSON.stringify(subscription))
-        var details = {subscription, account_id}
-        return axios.post(process.env.VUE_APP_API_BASE_URI + '/notify-api/subscriptions/subscribe',details)               
-      })
-      .then(result=>{
-        console.log(JSON.stringify(result.data))
-        this.sendWelcomeNofitication()
-      })
-      .catch(error=>{
-        console.error('error: ',error)
-      })
-  },
-  /**
-   * 
-   */
-  sendWelcomeNofitication(){
+  sendWelcomeNofitication() {
     this.registration.showNotification('FDA Portal v3.0', {
       body: 'You are now subscribed to FDA Portal Notifications',
       icon: '/img/icons/vue-logo.png'
     });
+  },
+
+  setSubscription(account_id, isSubscribe) {
+    this.isSubscribed = isSubscribe
+    if (!isSubscribe) {
+      return axios.get(process.env.VUE_APP_API_BASE_URI + `/notify-api/subscriptions/subscribe/remove/${account_id}`)
+    } else if (this.subscription) {
+      return this.saveSubscription(account_id, this)
+    } else {
+      return this.subscribe(account_id, this)
+    }
   }
 }
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {      
-    },
-    registered (registration) {
+    ready() {},
+    registered(registration) {
       //check for updates every 1hr
-      setInterval(() => {registration.update()}, 1000 * 60 * 60);
+      setInterval(() => {
+        registration.update()
+      }, 1000 * 60 * 60);
       //check permission
       window.fda_pwa.checkSubscription(registration)
       // console.log('Notification.permission', Notification.permission)
       // checkNotificationPermission(registration)
 
     },
-    cached () {
+    cached() {
       console.log('Content has been cached for offline use.')
     },
-    updatefound () {
+    updatefound() {
       console.log('New content is downloading.')
     },
-    updated (registration) {
+    updated(registration) {
       console.log('New content is available; please refresh.')
-      document.dispatchEvent(new CustomEvent('swUpdated', { detail: registration }));
+      document.dispatchEvent(new CustomEvent('swUpdated', {
+        detail: registration
+      }));
     },
-    offline (registration) {
+    offline(registration) {
       console.log('No internet connection found. App is running in offline mode.')
-      document.dispatchEvent(new CustomEvent('swOffline', { detail: registration }));
+      document.dispatchEvent(new CustomEvent('swOffline', {
+        detail: registration
+      }));
     },
-    error (error) {
+    error(error) {
       console.error('Error during service worker registration:', error)
     }
   })
 }
-
