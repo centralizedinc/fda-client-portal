1c42a56160f4a4d7fba03ccacb7c8aebee6ed108
VPBelo <venus.belo.rhi@gmail.com>
2019-06-27 14:45:27 +0800

    update

--- src/views/app/certificates/create/tabs/NutritionHealthClaims.vue
+++ src/views/app/certificates/create/tabs/NutritionHealthClaims.vue
@@ -34,7 +34,7 @@
                 <v-icon>far fa-minus-square</v-icon>
               </v-btn>Remove
             </v-tooltip>
-          </v-flex>        
+          </v-flex>
         </template>
       </v-layout>
     </v-container>
@@ -55,27 +55,25 @@ export default {
       required: value => !!value || "This field is required"
     }
   }),
-  created(){
-    this.init()
+  created() {
+    this.init();
   },
 
   methods: {
     addItem() {
-      console.log("add item claim first data: " + JSON.stringify(this.form.claims))
+      console.log(
+        "add item claim first data: " + JSON.stringify(this.form.claims)
+      );
       this.form.claims.push({
         claim: this.claims,
         desc: this.description
       });
-      this.claims = ""
-      this.description = ""
-      console.log("add item claim data: " + JSON.stringify(this.form.claims))
+      this.claims = "";
+      this.description = "";
+      console.log("add item claim data: " + JSON.stringify(this.form.claims));
     },
     proceed() {
-      if (this.validate()) {
-        this.$emit("next", 8);
-      } else {
-        this.$notifyError([{ message: "Fill-up required fields." }]);
-      }
+      this.$emit("next", 8);
     },
     cancel() {
       this.$emit("next", 6);
@@ -87,9 +85,12 @@ export default {
     removeItem(index) {
       this.form.claims.splice(index, 1);
     },
-    init(){
+    init() {
       // this.nutrition_health_claims = this.$store.state.foodCertificate.nutrition_health_claims
-      console.log("nutrition health claims data: " + JSON.stringify(this.nutrition_health_claims))
+      console.log(
+        "nutrition health claims data: " +
+          JSON.stringify(this.nutrition_health_claims)
+      );
     }
   }
 };
