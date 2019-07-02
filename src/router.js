bfb76602922771ea67abe8228f4c95a145be6ec9
VPBelo <venus.belo.rhi@gmail.com>
2019-07-02 10:17:42 +0800

    update

--- src/router.js
+++ src/router.js
@@ -123,36 +123,42 @@ var router = new Router({
         {
           path: 'licenses',
           name: 'License to Operate',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/licenses/Licenses.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'licenses/view',
           name: 'View License Application',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/licenses/View.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'licenses/renew',
           name: 'Renewal License Application',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/licenses/Renew.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'licenses/variation',
           name: 'Variate License Application',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/licenses/Variation.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'licenses/pay',
           name: 'Pay License',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/payment/PaymentSummary.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'certificate/pay',
           name: 'Pay Certificate',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/payment/PaymentSummary.vue'),
           // beforeEnter: isAuthorized
         },
@@ -166,54 +172,63 @@ var router = new Router({
         {
           path: 'certificates',
           name: 'Certificate of Product Registration',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/certificates/Certificates.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'certificates/renew',
           name: 'Renewal Certificate of Product Registration',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/certificates/Renew.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'certificates/apply',
           name: 'New Certificate of Product Registration',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/certificates/Apply.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'notification',
           name: 'Product Notification',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/notification/Notification.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'notification/apply',
           name: 'New Product Notification',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/notification/Apply.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'certificates/overview',
           name: 'Application Overview',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/certificates/AppOverview.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'certificates/variation',
           name: 'Application Amendment',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/certificates/Variation.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'payments',
           name: 'Payments',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/payment/Payment.vue'),
           // beforeEnter: isAuthorized
         },
         {
           path: 'payments/paylater',
           name: 'Pay Later',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/payment/PayLater.vue'),
           // beforeEnter: isAuthorized
         },
@@ -221,6 +236,7 @@ var router = new Router({
         {
           path: 'payments/creditcard',
           name: 'Credit Card Payment',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/payment/CreditCardPayment.vue'),
           // beforeEnter: isAuthorized
         },
@@ -246,6 +262,7 @@ var router = new Router({
         {
           path: 'logout',
           name: 'Logout',
+          beforeEnter: dropBreadcrumbs,
           component: () => import('@/views/app/UserPortfolio.vue')
         },
       ]
